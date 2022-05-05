b6167e2c4f52e55a287710b3f0725f196b40ec99
castilloglenn <castilloglenn@ymail.com>
2022-05-05 14:18:59 +0800

    quality of life additions

--- game/confirm_menu.py
+++ game/confirm_menu.py
@@ -94,8 +94,10 @@ class ConfirmMenu():
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self.background.enable = False
-            if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
-                self.confirm()
+            if event.key == pygame.K_RETURN \
+                or event.key == pygame.K_KP_ENTER \
+                or event.key == pygame.K_SPACE:
+                    self.confirm()
         elif event.type == pygame.MOUSEMOTION: 
             for button in self.hoverable_buttons:
                 button.check_hovered(event.pos)
