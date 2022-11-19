f42f19ae5f39c1d7fbe45ba5321e4396ccffdf70
Allen Glenn Castillo <castilloglenn@ymail.com>
2022-11-19 17:35:08 +0800

    added automated exe builder batch file
    - for alpha version only (dev)

---
+++ build_alpha.bat
@@ -0,0 +1,16 @@
+pyinstaller main.py --onefile
+move build release\alpha
+move dist release\alpha
+del main.spec
+mkdir "release\alpha\entrepinoy"
+move release\alpha\dist\main.exe release\alpha\entrepinoy
+mkdir release\alpha\entrepinoy\assets
+xcopy /e assets release\alpha\entrepinoy\assets\
+mkdir release\alpha\entrepinoy\game
+xcopy /e game release\alpha\entrepinoy\game\
+xcopy map.py release\alpha\entrepinoy
+xcopy scene_builder.py release\alpha\entrepinoy
+xcopy setting.py release\alpha\entrepinoy
+cd release\alpha\entrepinoy
+main
+cmd -k
