354cd1e9f4145616040928f13aa378876b0fa021
Allen Glenn Castillo <castilloglenn@ymail.com>
2022-12-05 00:51:27 +0800

    fix quick flash of business upon selling

--- game/sprite/business.py
+++ game/sprite/business.py
@@ -169,7 +169,9 @@ class Business(Button):
             self.name_code
         ]["has_employee"]
 
+        self.visible = False
         super().update()
+        self.visible = True
 
     def reconstruct(
         self,
