ae7f21f54ffdf04259a2a2422c4ea837997b5ce6
Allen Glenn Castillo <castilloglenn@ymail.com>
2022-12-18 18:47:09 +0800

    add base class descriptions for the last modules

---
+++ game/slideshow.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Slideshow:
+    """
+    Class to present both prologue and epilogue in a slideshow manner, showing
+    1 photo at a time with a certain duration and a next button.
+    """
+
+    def __init__(self) -> None:
+        pass
---
+++ game/tutorial_overlay.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class TutorialOverlay:
+    """
+    Class to include an overlay to the game that teaches certain aspects on how
+    the game shall be played.
+    """
+
+    def __init__(self) -> None:
+        pass
