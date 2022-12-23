c7e50fccd4130f2cd84d946bd0a025e1f26c448d
Allen Glenn Castillo <castilloglenn@ymail.com>
2022-12-24 02:29:09 +0800

    update profile backbone

--- game/profile.py
+++ game/profile.py
@@ -1,12 +1,46 @@
-from game.sprite.menu_background import MenuBackground
+from game.generic_menu import GenericMenu
+
+import pygame
 
 
-class BusinessMenu:
+class ProfileMenu(GenericMenu):
     """
     This class will handle menu that is related to the profile of the user
     containing their details, business status, financial status, and the game
     progress such as achievements and etc.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, main) -> None:
+        super().__init__(main)
+        # The parent class contains:
+        # handle_event(self, event)
+        # update(self)
+        # close(self)
+
+        # Instantiate logical variables
+        ...
+
+        # Instantiate buttons and objects
+        ...
+
+    # If reconstructable, add this function
+    # def reconstruct(self, args):
+    #     ...
+
+    # Internal functions here
+    # ...
+
+    # Abstract method implementation
+    def set_button_states(self):
+        ...
+
+    # Abstract method implementation
+    def update_data(self):
+        # This will be called in conjunction with the screen update to always
+        #   make the details in the business update and buttons will be enabled
+        #   when a sale is made and etc.
+        ...
+
+    # Abstract method implementation
+    def set_data(self):
+        ...
