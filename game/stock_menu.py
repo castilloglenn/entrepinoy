db86453e6e3ca5036cdc06833ce80a3275f30b0b
Allen Glenn Castillo <castilloglenn@ymail.com>
2022-12-24 02:14:39 +0800

    add stock menu backbone

---
+++ game/stock_menu.py
@@ -0,0 +1,44 @@
+from game.generic_menu import GenericMenu
+
+import pygame
+
+
+class StockMenu(GenericMenu):
+    """
+    Menu showing the simplified trading UI of stock trading.
+    """
+
+    def __init__(self, main) -> None:
+        super().__init__(main)
+        # The parent class contains:
+        # handle_event(self, event)
+        # update(self)
+        # close(self)
+
+        # Instantiate logical variables
+        ...
+
+        # Instantiate buttons and objects
+        ...
+
+    # If reconstructable, add this function
+    # def reconstruct(self, args):
+    #     ...
+
+    # Internal functions here
+    # ...
+
+    # Abstract method implementation
+    def set_button_states(self):
+        ...
+
+    # Abstract method implementation
+    def update_data(self):
+        # This will be called in conjunction with the screen update to always
+        #   make the details in the business update and buttons will be enabled
+        #   when a sale is made and etc.
+        ...
+
+    # Abstract method implementation
+    def set_data(self):
+        ...
