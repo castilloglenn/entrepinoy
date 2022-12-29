19666b928a69d87693210f1684b800e233d2089e
Allen Glenn Castillo <castilloglenn@ymail.com>
2022-12-29 12:06:10 +0800

    implement achievements logic

--- game/utility/achievement_menu.py
+++ game/utility/achievement_menu.py
@@ -3,6 +3,7 @@ from game.sprite.button import Button
 
 from game.utility.generic_menu import GenericMenu
 
+from numerize.numerize import numerize
 import pygame
 
 
@@ -19,7 +20,24 @@ class AchievementMenu(GenericMenu):
         # close(self)
 
         # Instantiate logical variables
-        self.data = self.main.data.progress["statistics"]
+        self.data = self.main.data.progress["achievements"]
+        self.pages = []
+        self.page_index = 0
+        self.page_total = 0
+
+        displayed_per_page = 3
+        page = []
+        for achievement in self.data:
+            page.append(achievement)
+            if len(page) >= displayed_per_page:
+                self.pages.append(page)
+                self.page_total += 1
+                page = []
+
+        if len(page) > 0:
+            self.pages.append(page)
+            self.page_total += 1
+
         self.obtain = self.main.data.meta_images[
             "achievement_trophy_true"
         ].convert_alpha()
@@ -44,8 +62,6 @@ class AchievementMenu(GenericMenu):
         self.bar = "============================================================"
         self.max_width = len("0123456789ABCDEFGHIJ0123456789ABCDEFGHIJ01234")
         self.width_center = int(self.max_width / 2)
-        self.page_index = 0
-        self.page_total = 2
 
         # Instantiate buttons and objects
         self.title_message = Message(
@@ -59,27 +75,9 @@ class AchievementMenu(GenericMenu):
             ),
         )
 
-        page_number = f"{self.page_index + 1}/{self.page_total}"
-        start_index = (self.width_center - (int(len(page_number) / 2))) - 1
-        line = " " * start_index + page_number + " " * start_index
-        self.mission_message = Message(
+        self.achievement_message = Message(
             self.screen,
-            [
-                "============================================================",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "============================================================",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "============================================================",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "     0123456789ABCDEFGHIJ012345678           ",
-                "============================================================",
-                f"{line}",
-            ],
+            [""],
             self.main.data.large_font,
             self.main.data.colors["brown"],
             top_left_coordinates=(
@@ -92,9 +90,9 @@ class AchievementMenu(GenericMenu):
             "2": [0.749, 0.445],
             "3": [0.749, 0.655],
         }
-        self.mission_1_collect_button = Button(
+        self.achievement_1_collect_button = Button(
             self.main,
-            lambda x: print("test"),
+            self._achievement_1_collect,
             top_left_coordinates=(
                 int(self.canvas_rect.width * coords["1"][0]) + self.canvas_rect.x,
                 int(self.canvas_rect.height * coords["1"][1]) + self.canvas_rect.y,
@@ -111,9 +109,9 @@ class AchievementMenu(GenericMenu):
                 ].convert_alpha(),
             },
         )
-        self.mission_2_collect_button = Button(
+        self.achievement_2_collect_button = Button(
             self.main,
-            lambda x: print("test"),
+            self._achievement_2_collect,
             top_left_coordinates=(
                 int(self.canvas_rect.width * coords["2"][0]) + self.canvas_rect.x,
                 int(self.canvas_rect.height * coords["2"][1]) + self.canvas_rect.y,
@@ -130,9 +128,9 @@ class AchievementMenu(GenericMenu):
                 ].convert_alpha(),
             },
         )
-        self.mission_3_collect_button = Button(
+        self.achievement_3_collect_button = Button(
             self.main,
-            lambda x: print("test"),
+            self._achievement_3_collect,
             top_left_coordinates=(
                 int(self.canvas_rect.width * coords["3"][0]) + self.canvas_rect.x,
                 int(self.canvas_rect.height * coords["3"][1]) + self.canvas_rect.y,
@@ -202,16 +200,87 @@ class AchievementMenu(GenericMenu):
     def _next_page(self, args):
         self.page_index = min(self.page_index + 1, self.page_total - 1)
 
+    def _achievement_1_collect(self, args):
+        if len(self.pages[self.page_index]) >= 1:
+            self.data[self.pages[self.page_index][0]]["obtained"] = True
+            self.main.data.progress["cash"] += self.data[
+                self.pages[self.page_index][0]
+            ]["reward"]
+            self.data[self.pages[self.page_index][0]]["reward"] = 0.0
+
+    def _achievement_2_collect(self, args):
+        if len(self.pages[self.page_index]) >= 2:
+            self.data[self.pages[self.page_index][1]]["obtained"] = True
+            self.main.data.progress["cash"] += self.data[
+                self.pages[self.page_index][1]
+            ]["reward"]
+            self.data[self.pages[self.page_index][1]]["reward"] = 0.0
+
+    def _achievement_3_collect(self, args):
+        if len(self.pages[self.page_index]) >= 3:
+            self.data[self.pages[self.page_index][2]]["obtained"] = True
+            self.main.data.progress["cash"] += self.data[
+                self.pages[self.page_index][2]
+            ]["reward"]
+            self.data[self.pages[self.page_index][2]]["reward"] = 0.0
+
     # Abstract method implementation
     def set_button_states(self):
-        ...
+        self.previous_button.set_disabled(self.page_index == 0)
+        self.next_button.set_disabled(self.page_index == self.page_total - 1)
+
+        if len(self.pages[self.page_index]) >= 1:
+            achievement_data = self.data[self.pages[self.page_index][0]]
+            if (
+                achievement_data["value"] == achievement_data["requirement"]
+                and not achievement_data["obtained"]
+            ):
+                self.achievement_1_collect_button.set_disabled(False)
+            else:
+                self.achievement_1_collect_button.set_disabled(True)
+
+        if len(self.pages[self.page_index]) >= 2:
+            achievement_data = self.data[self.pages[self.page_index][1]]
+            if (
+                achievement_data["value"] == achievement_data["requirement"]
+                and not achievement_data["obtained"]
+            ):
+                self.achievement_2_collect_button.set_disabled(False)
+            else:
+                self.achievement_2_collect_button.set_disabled(True)
+
+        if len(self.pages[self.page_index]) >= 3:
+            achievement_data = self.data[self.pages[self.page_index][2]]
+            if (
+                achievement_data["value"] == achievement_data["requirement"]
+                and not achievement_data["obtained"]
+            ):
+                self.achievement_3_collect_button.set_disabled(False)
+            else:
+                self.achievement_3_collect_button.set_disabled(True)
 
     # Abstract method implementation
     def update_data(self):
         # This will be called in conjunction with the screen update to always
         #   make the details in the business update and buttons will be enabled
         #   when a sale is made and etc.
+        message = [self.bar]
+        for achievement in self.pages[self.page_index]:
+            data = self.data[achievement]
+            value = numerize(data["value"])
+            achievement_desc = data["description"][:2]
+            progress = data["description"][2].format(value=value)
+            achievement_desc.append(progress)
 
+            message += achievement_desc
+            message.append(self.bar)
+
+        page_number = f"{self.page_index + 1}/{self.page_total}"
+        start_index = (self.width_center - (int(len(page_number) / 2))) - 1
+        line = " " * start_index + page_number + " " * start_index
+        message.append(line)
+
+        self.achievement_message.set_message(messages=message)
         self.set_button_states()
 
     def update(self):
@@ -219,26 +288,54 @@ class AchievementMenu(GenericMenu):
 
         # Render trophies images
         if self.background.enable:
-            self.main.screen.blit(self.obtain, self.achievement_1_rect)
-            self.main.screen.blit(self.not_obtain, self.achievement_2_rect)
-            self.main.screen.blit(self.obtain, self.achievement_3_rect)
+            if len(self.pages[self.page_index]) >= 1:
+                if self.data[self.pages[self.page_index][0]]["obtained"]:
+                    self.main.screen.blit(self.obtain, self.achievement_1_rect)
+                else:
+                    self.main.screen.blit(self.not_obtain, self.achievement_1_rect)
+
+            if len(self.pages[self.page_index]) >= 2:
+                if self.data[self.pages[self.page_index][1]]["obtained"]:
+                    self.main.screen.blit(self.obtain, self.achievement_2_rect)
+                else:
+                    self.main.screen.blit(self.not_obtain, self.achievement_2_rect)
+
+            if len(self.pages[self.page_index]) >= 3:
+                if self.data[self.pages[self.page_index][2]]["obtained"]:
+                    self.main.screen.blit(self.obtain, self.achievement_3_rect)
+                else:
+                    self.main.screen.blit(self.not_obtain, self.achievement_3_rect)
 
     # Abstract method implementation
     def set_data(self):
         super().set_data()
 
         self.title_message.add(self.objects)
-        self.mission_message.add(self.objects)
+        self.achievement_message.add(self.objects)
 
-        self.mission_1_collect_button.add(
+        self.achievement_1_collect_button.add(
             self.objects, self.buttons, self.hoverable_buttons, self.tooltips
         )
-        self.mission_2_collect_button.add(
+        self.achievement_2_collect_button.add(
             self.objects, self.buttons, self.hoverable_buttons, self.tooltips
         )
-        self.mission_3_collect_button.add(
+        self.achievement_3_collect_button.add(
             self.objects, self.buttons, self.hoverable_buttons, self.tooltips
         )
 
         self.previous_button.add(self.objects, self.buttons, self.hoverable_buttons)
         self.next_button.add(self.objects, self.buttons, self.hoverable_buttons)
+
+    def handle_event(self, event):
+        super().handle_event(event)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user scrolls the mouse wheel downward
+            if event.button == 4:
+                self.page_index = max(self.page_index - 1, 0)
+                return
+
+            # If the user scrolls the mouse wheel upward
+            if event.button == 5:
+                self.page_index = min(self.page_index + 1, self.page_total - 1)
+                return
