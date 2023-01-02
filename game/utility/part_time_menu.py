5e8cc1762a6961fe6846be03e348fbfafe1cb36a
Allen Glenn Castillo <castilloglenn@ymail.com>
2023-01-02 12:52:59 +0800

    randomize data entry length

--- game/utility/part_time_menu.py
+++ game/utility/part_time_menu.py
@@ -347,7 +347,7 @@ class PartTimeMenu(GenericMenu):
 
             self.word_set = []
             self.word_set_index = 0
-            self.word_set_size = 20
+            self.word_set_size = random.randint(10, 20)
             self.word_letters_sum = 0
 
             self.word_written = ""
