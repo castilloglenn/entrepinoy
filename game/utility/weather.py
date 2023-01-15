584f68ea2d4699035e713c27980a888438b4a478
Allen Glenn Castillo <castilloglenn@ymail.com>
2023-01-15 16:26:19 +0800

    create base format and documentation

---
+++ game/utility/weather.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Weather:
+    """Class to control unpredictable weather events in the game such as:
+    - Rainfall
+    - Heat wave
+    """
+
+    def __init__(self, main):
+        pass
